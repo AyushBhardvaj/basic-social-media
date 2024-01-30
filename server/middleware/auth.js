63e501c105aef12a538cba3a889e1171dbbfc833
Ayush Bhardvaj <ayushbhardvaj01@gmail.com>
2024-01-30 16:52:52 +0530

    asset changes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 const verifyToken = async (req, res, next) => {
   try {
     const { token } = req.cookies;
-
+    console.log(req.cookies);
     if (!token) {
       return res.status(403).send("Access Denied");
     }
@@ -12,10 +12,6 @@ const verifyToken = async (req, res, next) => {
     const user = await User.findById(decodedId.id);
     req.user = user;
     next();
-    // const verified = jwt.verify(token, process.env.JWT_SECRET);
-
-    // req.user = verified;
-    // next();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
